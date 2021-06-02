80df327c91ec05a4a3a0fe5ff88f256ada0081ef
Rashad-Muntar <muntartoure@gmail.com>
2021-06-02 20:38:47 +0000

    add new project logic

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,10 @@
+import projectFactory from './modules/project'
 
+
+const EXISTING_DATA = JSON.parse(localStorage.getItem('mainProjects')) || []
 
 let priorities = ['High', 'Medium', 'Low']
+let submitProject = document.getElementById('submit')
 
 const content = document.getElementById('content')
 
@@ -11,10 +15,19 @@ const projectForm = () => {
     let date = document.createElement('input')
     let select = document.createElement('select')
     let textarea = document.createElement('textarea')
+    let submit = document.createElement('input')
 
     input.setAttribute('type', 'text')
-    input.setAttribute('placeholder', 'Enter Project Title')
     date.setAttribute('type', 'date')
+    submit.setAttribute('type', 'submit')
+    submit.value = 'New project'
+    input.setAttribute('placeholder', 'Enter Project Title')
+
+    input.setAttribute('id', 'title')
+    date.setAttribute('id', 'date')
+    select.setAttribute('id', 'priority')
+    textarea.setAttribute('id', 'desc')
+    submit.setAttribute('id', 'submit')
 
 
     for(let i  = 0; i  < priorities.length; i++){
@@ -29,13 +42,31 @@ const projectForm = () => {
     form.appendChild(date)
     form.appendChild(textarea)
     form.appendChild(select)
+    form.appendChild(submit)
    
     projectForm.appendChild(form)
     content.appendChild(projectForm)
 }
 
-const newProject = () => {
+const projectConstructor = (e) => {
+    e.preventDefault()
 
+  let newProject = projectFactory(
+    document.getElementById('title').value,
+    document.getElementById('date').value,
+    document.getElementById('priority').value,
+    document.getElementById('desc').value
+  )
+
+    EXISTING_DATA.push(newProject)
+    localStorage.setItem('mainProjects', JSON.stringify(EXISTING_DATA))
 }
 
-export { projectForm }
+
+    submitProject.addEventListener('click', ()=>{
+        console.log()
+    })
+
+
+
+export { projectForm, projectConstructor}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './main.scss';
 
-import { projectFactory } from './modules/project'
 import { projectForm } from './dom'
 
 projectForm()
